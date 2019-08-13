df441a8a42fbf7bfa6bb71523f5990ce070631c3
rizal271 <rizalrohman23@gmail.com>
2019-08-13 16:01:53 +0700

    add multer

--- src/routes/users.js
+++ src/routes/users.js
@@ -9,7 +9,7 @@ const multer = require('multer');
 const storage = multer.diskStorage({
   destination: function(req, file, callback) {
     callback(null, './uploads');
-  },
+  }, 
   filename: function(req, file, callback) {
     callback(null, file.originalname);
   }
